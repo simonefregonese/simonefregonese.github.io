3fe205e26c34fa07d3f51e7255cd6a8c73b519af
Simone Fregonese <Simone Fregonese>
2016-05-13 12:35:56 +0200

    ijouhugi

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,15 +2,20 @@
  * Created by Sim2 on 12/05/2016.
  */
 
-var app = angular.module('myApp', [])
-    .controller('homeController', function($scope){
+var app = angular.module('myApp', ['ngStorage'])
+    .controller('homeController', function($scope, $localStorage, $sessionStorage){
 
         $scope.result = [];
 
         $scope.invia = function(testo){
 
             $scope.result.push(testo);
+            $scope.data = new Date();
+            $localStorage.result = $scope.result;
+            $sessionStorage.result = $scope.result;
         };
+
+        $
     });
 
     app.filter('reverse', function() {
